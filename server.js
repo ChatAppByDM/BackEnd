f39c24d9d56170c799f75012d8cfe33d4488a34f
debargham14 <debargham14@gmail.com>
2022-02-04 12:42:55 +0530

    changed cors url

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chat-app-by-dm.web.app",
+    origin: "https://frontend-nlz2bl0jc-chat-app-by-dm.vercel.app",
     // credentials: true,
   },
 });
